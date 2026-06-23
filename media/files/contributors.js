4b26a16708e4a4816e7e4aaf63a54c345d8f7397
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2026-06-23 11:52:46 -0500

    Updated from Publii

---
+++ media/files/contributors.js
@@ -0,0 +1,20 @@
+window.CONTRIBUTORS_DATA = [
+  {
+    "name": "Sergio Rodríguez",
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend nulla odio, et placerat nisi rutrum ut. Donec faucibus faucibus sapien, nec placerat massa condimentum sed. ",
+    "email": "aaa@gmail.com",
+    "url": "https://srsergiorodriguez.github.io/"
+  },
+  {
+    "name": "Danilo Penagos",
+    "bio": "Vestibulum elit massa, pulvinar porttitor malesuada vel, finibus non ex. Suspendisse ornare luctus diam sit amet pellentesque.",
+    "email": "bbb@gmail.com",
+    "url": "https://srsergiorodriguez.github.io/"
+  },
+  {
+    "name": "Hanno Erlich",
+    "bio": "Praesent tellus nunc, maximus vel nisl in, cursus interdum diam. Aliquam rhoncus arcu eu quam porta, at cursus elit aliquam. Fusce et mauris malesuada, ornare est nec, imperdiet dolor. Vestibulum gravida molestie turpis vitae aliquet.",
+    "email": "ccc@gmail.com",
+    "url": "https://srsergiorodriguez.github.io/"
+  }
+];
